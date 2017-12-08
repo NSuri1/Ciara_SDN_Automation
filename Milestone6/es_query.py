10db3d20ae92a0f50e67187fe29d92a6833f4dcf
Nalin Suri <nalinsuri@gmail.com>
2017-12-08 13:01:30 -0500

    Create a class to filter searches by commodity vlans

---
+++ Milestone6/es_query.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+from elasticsearch import Elasticsearch, client
+from datetime import date, timedelta
+from phpIPAM import phpIPAM
+import json
+
+
+class ElasticQuery:
+    'Class to query elasticsearch'
+
+    def __init__(self, es_obj, ipam_obj=None):
+        """
+        Class constructor
+        :param es_obj: Elasticsearch object
+        :param ipam_obj: phpIPAM object
+        """
+        self.es_obj = es_obj
+        self.ipam_obj = ipam_obj
+
+
+    def __init__(self, hostname, ipam_obj=None):
+        """
+        Class constructor
+        :param es_obj: Elasticsearch location string
+        :param ipam_obj: phpIPAM object
+        """
+        self.es_obj = Elasticsearch(hostname, timeout=100)
+        self.ipam_obj = ipam_obj
+
+    def print_obj(self):
+        """
+        Print Elasticsearch and phpIPAM object information
+        """
+        print('Elasticsearch object: %s' % self.es_obj)
+        print('phpIPAM object: %s' % self.ipam_obj)
+
+    def get_es_obj(self):
+        """
+        Get access to Elasticsearch object directly.
+        This allows you to make use of the Elasticsearch python
+        API directly. For more info about the ES API, visit 
+        https://elasticsearch-py.readthedocs.io/en/master/api.html
+        :return: Elasticsearch object
+        """
+        return self.es_obj
+
+    def get_traffic(self, filter_vlans=False):
+        body = {
+            "query" : {
+                "bool": {
+                    "must": [{
+                        "query_string": {
+                            "query": "*",
+                            "analyze_wildcard": True
+                        }
+                    }],
+                    "must_not": []
+                }
+            },
+            "size": 0,
+            "aggs": {
+                "1": {
+                    "sum": {
+                        "field": "PacketSize",
+                        "script": "doc[\"PacketSize\"].value * doc[\"SampleRate\"].value"
+                    }
+                }
+            }
+        }
+
+        if filter_vlans:
+            vlan_list = self.get_commodity_vlan_ids(self.ipam_obj)
+            body['query']['bool']['must_not'].append({
+                "terms" : { 
+                    "in_vlan" : vlan_list
+                }
+            })
+
+        index = 'sflow-%s' % self.get_yesterday_date()
+        
+        return self.es_obj.search(index=index, body=body)['aggregations']['1']['value'] / 1000000000000
+
+    def top_ten_talkers(self):
+        pass
+
+    def filter_by_vlan(self, filtered_vlans):
+        pass
+
+    def get_yesterday_date(self):
+        return (date.today() - timedelta(1)).strftime('%Y.%m.%d')
+
+    def get_commodity_vlan_ids(self, ipam):
+        """
+        Get a list of all commodity vlan ids
+        :param ipam: phpIPAM object
+        :return: list of commodity vlan ids
+        """
+
+        vlan_list = ipam.vlan_get_all()
+        commodity_vlan_ids = []
+
+        try:
+            vlan_list = vlan_list["data"]
+            for vlan in vlan_list:
+                if int(vlan["isAcademic"]) is 0:
+                    commodity_vlan_ids.append(int(vlan["number"]))
+            return commodity_vlan_ids
+        except Exception as e:
+            print("FAILURE: %s" % e)
---
+++ Milestone6/query_test.py
@@ -0,0 +1,34 @@
+from es_query import ElasticQuery
+import configparser
+from phpIPAM import phpIPAM
+
+def read_config_file(config_file):
+    """
+    Create an instance of configparser
+    :param config_file: location of config file
+    :return: instance of ConfigParser
+    """
+
+    # Get necessary credentials from ini file
+    config = configparser.ConfigParser()
+    try:
+        with open(config_file) as f:
+            config.read_file(f)
+    except Exception as err:
+        print err
+
+    return config
+
+
+config = read_config_file("./Credentials.txt")
+username = config.get("phpIPAM", "Username")
+password = config.get("phpIPAM", "Password")
+
+es_location = config.get("Elasticsearch", "Hostname")
+
+ipam = phpIPAM("https://apps.amlight.net/phpipam", "amlight",
+               username, password)
+
+test = ElasticQuery(es_location, ipam)
+
+print(test.get_traffic(filter_vlans=False))
